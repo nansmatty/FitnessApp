fe838b926f005c09525194e445b2066b4248d8eb
nansmatty <narayanmaitysp1997@gmail.com>
2022-06-14 17:36:03 +0530

    basic structure done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import { Box } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import ExerciseDetails from './pages/ExerciseDetails';
+import Home from './pages/Home';
 
 const App = () => {
 	return (
-		<div>
-			<h1>Hello World.</h1>
-		</div>
+		<Box width='400px' sx={{ width: { xl: '1488px' } }} m='auto'>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/exercise/:id' element={<ExerciseDetails />} />
+			</Routes>
+			<Footer />
+		</Box>
 	);
 };
 
---
+++ src/pages/ExerciseDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExerciseDetails = () => {
+	return (
+		<div>
+			<h1>ExerciseDetails</h1>
+		</div>
+	);
+};
+
+export default ExerciseDetails;
