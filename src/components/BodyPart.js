9d792af191cb7487a1141a1834d91e840135dd95
nansmatty <narayanmaitysp1997@gmail.com>
2022-06-19 16:25:21 +0530

    bodypart component done

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -17,6 +17,10 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
 				height: '280px',
 				cursor: 'pointer',
 				gap: '47px',
+			}}
+			onClick={() => {
+				setBodyPart(item);
+				window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
 			}}>
 			<img
 				src={Icon}
